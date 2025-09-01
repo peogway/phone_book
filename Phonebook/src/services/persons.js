2bb15938448c231349a77d29e154a64107e51d32
peogway <duchungbeo2004@gmail.com>
2025-09-01 06:40:02 +0300

    baseUrl fixed

--- Phonebook/src/services/persons.js
+++ Phonebook/src/services/persons.js
@@ -1,11 +1,14 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
-const getAll = () => axios.get(baseUrl).then(response => response.data)
+const getAll = () => axios.get(baseUrl).then((response) => response.data)
 
-const create = (newPerson) => axios.post(baseUrl, newPerson).then(response => response.data)
+const create = (newPerson) =>
+	axios.post(baseUrl, newPerson).then((response) => response.data)
 
-const deletePerson = (person) => axios.delete(`${baseUrl}/${person.id}`).then(response => response.data)
+const deletePerson = (person) =>
+	axios.delete(`${baseUrl}/${person.id}`).then((response) => response.data)
 
-const update =(id, updatedPerson) => axios.put(`${baseUrl}/${id}`, updatedPerson).then(response => response.data)
-export default {getAll, create, deletePerson, update}
+const update = (id, updatedPerson) =>
+	axios.put(`${baseUrl}/${id}`, updatedPerson).then((response) => response.data)
+export default { getAll, create, deletePerson, update }
