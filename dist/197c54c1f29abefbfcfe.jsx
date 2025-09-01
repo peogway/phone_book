214ed682ee60ff7a139e2c86b1a9c93f29b6d3cd
peogway <duchungbeo2004@gmail.com>
2025-09-01 11:20:02 +0300

    commit dist folder

---
+++ dist/197c54c1f29abefbfcfe.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom/client';
+/* eslint-disable no-unused-vars */
+import App from './App';
+import './index.css';
+ReactDOM.createRoot(document.getElementById('root')).render(/*#__PURE__*/React.createElement(App, null));
