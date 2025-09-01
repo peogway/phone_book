28d2ed0b62d7c35d622879c1300afcc5231e8acc
peogway <duchungbeo2004@gmail.com>
2025-09-01 10:47:23 +0300

    import React

--- Phonebook/src/main.jsx
+++ Phonebook/src/main.jsx
@@ -1,5 +1,6 @@
-import ReactDOM from 'react-dom/client'
 /* eslint-disable no-unused-vars */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
