fa7e839c5c98e80e1a1f451e3e92cf232ec1142f
peogway <duchungbeo2004@gmail.com>
2025-09-01 10:09:34 +0300

    cypress configuration

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+	e2e: {
+		setupNodeEvents(on, config) {},
+		baseUrl: 'http://localhost:3001',
+		specPattern: 'e2e-tests/**/*.cy.js',
+		supportFile: false,
+	},
+})
