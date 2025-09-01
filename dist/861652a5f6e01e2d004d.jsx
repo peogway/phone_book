f090d261d8e8c1d8a8a2986d7e20a3801adebf0a
peogway <duchungbeo2004@gmail.com>
2025-09-02 01:52:15 +0300

    delete

--- dist/861652a5f6e01e2d004d.jsx
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-ReactDOM.createRoot(document.getElementById('root')).render(/*#__PURE__*/React.createElement(App, null));
